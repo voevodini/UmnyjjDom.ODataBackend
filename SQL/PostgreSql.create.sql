c461bd523a68d1b07b84e067c4fc1f9999306bda
voevodini <emberplugin@mail.ru>
2020-08-20 10:38:12 +0000

    Flexberry code generation

--- SQL/PostgreSql.create.sql
+++ SQL/PostgreSql.create.sql
@@ -115,8 +115,6 @@ CREATE TABLE Помещение (
 
  Дом_m0 UUID NOT NULL,
 
- АдресныйРеестр_m0 UUID NOT NULL,
-
  Собственник_m0 UUID NOT NULL,
 
  PRIMARY KEY (primaryKey));
@@ -132,8 +130,6 @@ CREATE TABLE Дом (
  summS INT NULL,
 
  УправляющаяКомпания_m0 UUID NOT NULL,
-
- АдресныйРеестр_m0 UUID NOT NULL,
 
  PRIMARY KEY (primaryKey));
 
@@ -180,10 +176,6 @@ CREATE TABLE УправляющаяКомпания (
 CREATE TABLE ДомПоставщик (
 
  primaryKey UUID NOT NULL,
-
- Дом_m0 UUID NOT NULL,
-
- Поставщик_m0 UUID NOT NULL,
 
  PRIMARY KEY (primaryKey));
 
@@ -450,69 +442,57 @@ CREATE TABLE ApplicationLog (
 
 
 
- ALTER TABLE Тариф ADD CONSTRAINT FKd096fbb2fc1d41ea9daf42da65441bf7 FOREIGN KEY (ВидУслуги_m0) REFERENCES ВидУслуги; 
+ ALTER TABLE Тариф ADD CONSTRAINT FK3d3e097caade495086927efa3236e811 FOREIGN KEY (ВидУслуги_m0) REFERENCES ВидУслуги; 
 CREATE INDEX Index4efe9f21f1565b469b033b23b22cc953d6c41527 on Тариф (ВидУслуги_m0); 
 
- ALTER TABLE Тариф ADD CONSTRAINT FK756237b8b8f5486aa7a216a5fcf0dbbd FOREIGN KEY (Поставщик_m0) REFERENCES Поставщик; 
+ ALTER TABLE Тариф ADD CONSTRAINT FKefd0ecb5439e45ad818d7ba035f16e71 FOREIGN KEY (Поставщик_m0) REFERENCES Поставщик; 
 CREATE INDEX Index33c10eeb7288c970ebdf38051c0eb48f55cb7bff on Тариф (Поставщик_m0); 
 
- ALTER TABLE ПоказаниеПУ ADD CONSTRAINT FK2da871d21acb4708ac5ebfebfa60c28e FOREIGN KEY (ПриборУчета_m0) REFERENCES ПриборУчета; 
+ ALTER TABLE ПоказаниеПУ ADD CONSTRAINT FK6291b0eecd624e978951251a387480b7 FOREIGN KEY (ПриборУчета_m0) REFERENCES ПриборУчета; 
 CREATE INDEX Indexd6cce9d38823a1c432711a50b19373503b5d68d2 on ПоказаниеПУ (ПриборУчета_m0); 
 
- ALTER TABLE ПомещениеСобственника ADD CONSTRAINT FK9fa6a58807394f9c80e23c5a0f3b1eb6 FOREIGN KEY (Помещение_m0) REFERENCES Помещение; 
+ ALTER TABLE ПомещениеСобственника ADD CONSTRAINT FKb5f29345c812494db673e62c6bd52bfc FOREIGN KEY (Помещение_m0) REFERENCES Помещение; 
 CREATE INDEX Index61299f76ff53ff3270ca002f2a3d3427da2213a8 on ПомещениеСобственника (Помещение_m0); 
 
- ALTER TABLE ПомещениеСобственника ADD CONSTRAINT FK72009537584b4f32a0dc7169a7ee225a FOREIGN KEY (Собственник_m0) REFERENCES Собственник; 
+ ALTER TABLE ПомещениеСобственника ADD CONSTRAINT FK19ee0ad740ec48fca69e14dad982cc93 FOREIGN KEY (Собственник_m0) REFERENCES Собственник; 
 CREATE INDEX Index0fe3d46f06fe93ab18c33cae122a904d334f71c3 on ПомещениеСобственника (Собственник_m0); 
 
- ALTER TABLE Помещение ADD CONSTRAINT FKa3755910fd614e5a97f5891c8a20ace4 FOREIGN KEY (Дом_m0) REFERENCES Дом; 
+ ALTER TABLE Помещение ADD CONSTRAINT FKeee38e524a00442daf8f968f124e3fbc FOREIGN KEY (Дом_m0) REFERENCES Дом; 
 CREATE INDEX Indexf97e664ca029e850560b5496b12896dde2455ab2 on Помещение (Дом_m0); 
 
- ALTER TABLE Помещение ADD CONSTRAINT FK5767bda56ae0420f803ee456b299e359 FOREIGN KEY (АдресныйРеестр_m0) REFERENCES АдресныйРеестр; 
-CREATE INDEX Indexfe72dfb9a9ea657d02bd301b6c7deb9364f43144 on Помещение (АдресныйРеестр_m0); 
-
- ALTER TABLE Помещение ADD CONSTRAINT FK4b84943620b3468abbed6b3a2e3a5f00 FOREIGN KEY (Собственник_m0) REFERENCES Собственник; 
+ ALTER TABLE Помещение ADD CONSTRAINT FKcd6d74662ee34aa492042c1aa867fad8 FOREIGN KEY (Собственник_m0) REFERENCES Собственник; 
 CREATE INDEX Index88ae7359508e1b0f29ac3d6391b1ada245fdbe20 on Помещение (Собственник_m0); 
 
- ALTER TABLE Дом ADD CONSTRAINT FKad5d899bc6ef48d0a4ff16e65d331673 FOREIGN KEY (УправляющаяКомпания_m0) REFERENCES УправляющаяКомпания; 
+ ALTER TABLE Дом ADD CONSTRAINT FK1f6bfd7922144ad9bce59f7c3bc5aa1b FOREIGN KEY (УправляющаяКомпания_m0) REFERENCES УправляющаяКомпания; 
 CREATE INDEX Index8cc613dc87087d9dacaeec621207a2e90de170f1 on Дом (УправляющаяКомпания_m0); 
 
- ALTER TABLE Дом ADD CONSTRAINT FK33878f560e2c4fe38c88b35b904524da FOREIGN KEY (АдресныйРеестр_m0) REFERENCES АдресныйРеестр; 
-CREATE INDEX Index849aee09df438a952692e001c656b0d98898d9d4 on Дом (АдресныйРеестр_m0); 
-
- ALTER TABLE Сотрудник ADD CONSTRAINT FK5c19d1cc05d9454a9e35e9bb4ed1253a FOREIGN KEY (УправляющаяКомпания_m0) REFERENCES УправляющаяКомпания; 
+ ALTER TABLE Сотрудник ADD CONSTRAINT FK7dbde4a27db7498eb8eee6760c48f0cb FOREIGN KEY (УправляющаяКомпания_m0) REFERENCES УправляющаяКомпания; 
 CREATE INDEX Indexf69f79f9f32939c432502ebf4f1d9ff00132ef66 on Сотрудник (УправляющаяКомпания_m0); 
 
- ALTER TABLE ДомПоставщик ADD CONSTRAINT FK43b0ab02d6004ab9abf8b048e7b8eda0 FOREIGN KEY (Дом_m0) REFERENCES Дом; 
-CREATE INDEX Index395527b359b8a755046b8085885e36bc7ea4772a on ДомПоставщик (Дом_m0); 
-
- ALTER TABLE ДомПоставщик ADD CONSTRAINT FK99b29baa7de648c7b84812866a020027 FOREIGN KEY (Поставщик_m0) REFERENCES Поставщик; 
-CREATE INDEX Index474328d8c86a68c4ecc2cdcbcb507d7272335bb7 on ДомПоставщик (Поставщик_m0); 
-
- ALTER TABLE ПриборУчета ADD CONSTRAINT FK9edfeea1d1a9480b92333d032bed8b11 FOREIGN KEY (МодельПУ_m0) REFERENCES МодельПУ; 
+ ALTER TABLE ПриборУчета ADD CONSTRAINT FKedff493356e84b70aec375c2b67601ca FOREIGN KEY (МодельПУ_m0) REFERENCES МодельПУ; 
 CREATE INDEX Index0b5e9eb2d31d78044e491bbfb1691aef8746951a on ПриборУчета (МодельПУ_m0); 
 
- ALTER TABLE ПриборУчета ADD CONSTRAINT FK6727b26bcf2845ef8a362dab44e7583b FOREIGN KEY (Помещение_m0) REFERENCES Помещение; 
+ ALTER TABLE ПриборУчета ADD CONSTRAINT FK6cdeecd60d07478e9a84149658ec1cd4 FOREIGN KEY (Помещение_m0) REFERENCES Помещение; 
 CREATE INDEX Index03e59a2c17a211fa9af9bb9fd79463203fcd2558 on ПриборУчета (Помещение_m0); 
 
- ALTER TABLE Квитанция ADD CONSTRAINT FKc83fdf5bced040e5870213db46f7ec4e FOREIGN KEY (Помещение_m0) REFERENCES Помещение; 
+ ALTER TABLE Квитанция ADD CONSTRAINT FK274849cc1b554eae86b0b2d5783f5372 FOREIGN KEY (Помещение_m0) REFERENCES Помещение; 
 CREATE INDEX Index3b2475a1dea2a575569bb051ca87c1ad7c239647 on Квитанция (Помещение_m0); 
 
- ALTER TABLE Квитанция ADD CONSTRAINT FK44d0a5b808c448b18c31a10cf2f04145 FOREIGN KEY (УправляющаяКомпания_m0) REFERENCES УправляющаяКомпания; 
+ ALTER TABLE Квитанция ADD CONSTRAINT FK3a66768957a04f1a971caed6cedfb147 FOREIGN KEY (УправляющаяКомпания_m0) REFERENCES УправляющаяКомпания; 
 CREATE INDEX Index8a726da6bb6ef494f0c35f58a7ff42b4efbb9625 on Квитанция (УправляющаяКомпания_m0); 
 
- ALTER TABLE СтрокаКвитанции ADD CONSTRAINT FK1abd92e555274064a1f26a95db442085 FOREIGN KEY (ПоказаниеПУ_m0) REFERENCES ПоказаниеПУ; 
+ ALTER TABLE СтрокаКвитанции ADD CONSTRAINT FKc241d985a6294a008be8df8cd3c16695 FOREIGN KEY (ПоказаниеПУ_m0) REFERENCES ПоказаниеПУ; 
 CREATE INDEX Index3b33ecdbea4a900de7aae2210c993278d855a0d8 on СтрокаКвитанции (ПоказаниеПУ_m0); 
 
- ALTER TABLE СтрокаКвитанции ADD CONSTRAINT FK31d1e66babee43a2b81b3cd84fe8c19f FOREIGN KEY (Тариф_m0) REFERENCES Тариф; 
+ ALTER TABLE СтрокаКвитанции ADD CONSTRAINT FKab78acce909b4af185f0d584853f9e4a FOREIGN KEY (Тариф_m0) REFERENCES Тариф; 
 CREATE INDEX Index703ba542e585241f1ff80d29061fe379b2308d1f on СтрокаКвитанции (Тариф_m0); 
 
- ALTER TABLE СтрокаКвитанции ADD CONSTRAINT FK1e9bcd02efa14187b9c5558e8217b905 FOREIGN KEY (Квитанция_m0) REFERENCES Квитанция; 
+ ALTER TABLE СтрокаКвитанции ADD CONSTRAINT FKaa092083649049319d2867721bae6160 FOREIGN KEY (Квитанция_m0) REFERENCES Квитанция; 
 CREATE INDEX Indexce88417d4864090c9df2be074537be19db2fb5a7 on СтрокаКвитанции (Квитанция_m0); 
 
- ALTER TABLE STORMWEBSEARCH ADD CONSTRAINT FK2f089981797545d69dda3016e24637d7 FOREIGN KEY (FilterSetting_m0) REFERENCES STORMFILTERSETTING; 
+ ALTER TABLE STORMWEBSEARCH ADD CONSTRAINT FK2be405e7555541a2b400136696552199 FOREIGN KEY (FilterSetting_m0) REFERENCES STORMFILTERSETTING; 
 
- ALTER TABLE STORMFILTERDETAIL ADD CONSTRAINT FK1fc221d003164423b73a6bd3c05fd4e0 FOREIGN KEY (FilterSetting_m0) REFERENCES STORMFILTERSETTING; 
+ ALTER TABLE STORMFILTERDETAIL ADD CONSTRAINT FK5e6afaecaf3f4c84a964d0cd8ce92fb7 FOREIGN KEY (FilterSetting_m0) REFERENCES STORMFILTERSETTING; 
 
- ALTER TABLE STORMFILTERLOOKUP ADD CONSTRAINT FK068e2a882e944d2fb4c15e92b73a484f FOREIGN KEY (FilterSetting_m0) REFERENCES STORMFILTERSETTING; 
+ ALTER TABLE STORMFILTERLOOKUP ADD CONSTRAINT FKe19591f4c10c4201b2768b06673aa036 FOREIGN KEY (FilterSetting_m0) REFERENCES STORMFILTERSETTING; 
 
