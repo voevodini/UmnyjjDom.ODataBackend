7882a5b6f1bebcde6577591fc02c1211e355b0da
voevodini <emberplugin@mail.ru>
2020-08-20 10:13:48 +0000

    Flexberry code generation

---
+++ SQL/MSSql.create.sql
@@ -0,0 +1,493 @@
+﻿
+
+
+
+CREATE TABLE [Поставщик] (
+
+	 [primaryKey] UNIQUEIDENTIFIER  NOT NULL,
+
+	 [ИНН] VARCHAR(255)  NULL,
+
+	 [Наименование] VARCHAR(255)  NULL,
+
+	 [РасчетныйСчет] INT  NULL,
+
+	 [Описание] VARCHAR(255)  NULL,
+
+	 PRIMARY KEY ([primaryKey]))
+
+
+CREATE TABLE [ВидУслуги] (
+
+	 [primaryKey] UNIQUEIDENTIFIER  NOT NULL,
+
+	 [Наименование] VARCHAR(255)  NULL,
+
+	 [Ресурс] VARCHAR(255)  NULL,
+
+	 [Актуальность] BIT  NULL,
+
+	 PRIMARY KEY ([primaryKey]))
+
+
+CREATE TABLE [Собственник] (
+
+	 [primaryKey] UNIQUEIDENTIFIER  NOT NULL,
+
+	 [ФИО] VARCHAR(255)  NULL,
+
+	 [идТелеПлюс] INT  NULL,
+
+	 PRIMARY KEY ([primaryKey]))
+
+
+CREATE TABLE [Тариф] (
+
+	 [primaryKey] UNIQUEIDENTIFIER  NOT NULL,
+
+	 [Наименование] VARCHAR(255)  NULL,
+
+	 [Описание] VARCHAR(255)  NULL,
+
+	 [ДатаНачала] date  NULL,
+
+	 [ДатаОкончания] date  NULL,
+
+	 [Единица] VARCHAR(255)  NULL,
+
+	 [Формула] VARCHAR(255)  NULL,
+
+	 [Стоимость] VARCHAR(255)  NULL,
+
+	 [ВидУслуги_m0] UNIQUEIDENTIFIER  NOT NULL,
+
+	 [Поставщик_m0] UNIQUEIDENTIFIER  NOT NULL,
+
+	 PRIMARY KEY ([primaryKey]))
+
+
+CREATE TABLE [ПоказаниеПУ] (
+
+	 [primaryKey] UNIQUEIDENTIFIER  NOT NULL,
+
+	 [Значение] INT  NULL,
+
+	 [Дата] date  NULL,
+
+	 [ПолученоАвтоматически] BIT  NULL,
+
+	 [ПриборУчета_m0] UNIQUEIDENTIFIER  NOT NULL,
+
+	 PRIMARY KEY ([primaryKey]))
+
+
+CREATE TABLE [ПомещениеСобственника] (
+
+	 [primaryKey] UNIQUEIDENTIFIER  NOT NULL,
+
+	 [ОсновнойСобственник] VARCHAR(255)  NULL,
+
+	 [ДоляСобственности] VARCHAR(255)  NULL,
+
+	 [Помещение_m0] UNIQUEIDENTIFIER  NOT NULL,
+
+	 [Собственник_m0] UNIQUEIDENTIFIER  NOT NULL,
+
+	 PRIMARY KEY ([primaryKey]))
+
+
+CREATE TABLE [Помещение] (
+
+	 [primaryKey] UNIQUEIDENTIFIER  NOT NULL,
+
+	 [Площадь] INT  NULL,
+
+	 [ТипПомещения] VARCHAR(255)  NULL,
+
+	 [КолвоПроживающих] VARCHAR(255)  NULL,
+
+	 [Дом_m0] UNIQUEIDENTIFIER  NOT NULL,
+
+	 [АдресныйРеестр_m0] UNIQUEIDENTIFIER  NOT NULL,
+
+	 [Собственник_m0] UNIQUEIDENTIFIER  NOT NULL,
+
+	 PRIMARY KEY ([primaryKey]))
+
+
+CREATE TABLE [Дом] (
+
+	 [primaryKey] UNIQUEIDENTIFIER  NOT NULL,
+
+	 [Address] VARCHAR(255)  NULL,
+
+	 [summS] INT  NULL,
+
+	 [УправляющаяКомпания_m0] UNIQUEIDENTIFIER  NOT NULL,
+
+	 [АдресныйРеестр_m0] UNIQUEIDENTIFIER  NOT NULL,
+
+	 PRIMARY KEY ([primaryKey]))
+
+
+CREATE TABLE [МодельПУ] (
+
+	 [primaryKey] UNIQUEIDENTIFIER  NOT NULL,
+
+	 [Наименование] VARCHAR(255)  NULL,
+
+	 [Производитель] VARCHAR(255)  NULL,
+
+	 [Актуальность] BIT  NULL,
+
+	 PRIMARY KEY ([primaryKey]))
+
+
+CREATE TABLE [Сотрудник] (
+
+	 [primaryKey] UNIQUEIDENTIFIER  NOT NULL,
+
+	 [ФИО] VARCHAR(255)  NULL,
+
+	 [УправляющаяКомпания_m0] UNIQUEIDENTIFIER  NOT NULL,
+
+	 PRIMARY KEY ([primaryKey]))
+
+
+CREATE TABLE [УправляющаяКомпания] (
+
+	 [primaryKey] UNIQUEIDENTIFIER  NOT NULL,
+
+	 [Наименование] VARCHAR(255)  NULL,
+
+	 [ИНН] VARCHAR(255)  NULL,
+
+	 PRIMARY KEY ([primaryKey]))
+
+
+CREATE TABLE [ДомПоставщик] (
+
+	 [primaryKey] UNIQUEIDENTIFIER  NOT NULL,
+
+	 [Дом_m0] UNIQUEIDENTIFIER  NOT NULL,
+
+	 [Поставщик_m0] UNIQUEIDENTIFIER  NOT NULL,
+
+	 PRIMARY KEY ([primaryKey]))
+
+
+CREATE TABLE [ПриборУчета] (
+
+	 [primaryKey] UNIQUEIDENTIFIER  NOT NULL,
+
+	 [Единица] VARCHAR(255)  NULL,
+
+	 [ДатаУстановки] date  NULL,
+
+	 [ДатаСнятия] date  NULL,
+
+	 [Актуальность] BIT  NULL,
+
+	 [АвтоматическаяПередача] BIT  NULL,
+
+	 [МодельПУ_m0] UNIQUEIDENTIFIER  NOT NULL,
+
+	 [Помещение_m0] UNIQUEIDENTIFIER  NOT NULL,
+
+	 PRIMARY KEY ([primaryKey]))
+
+
+CREATE TABLE [АдресныйРеестр] (
+
+	 [primaryKey] UNIQUEIDENTIFIER  NOT NULL,
+
+	 PRIMARY KEY ([primaryKey]))
+
+
+CREATE TABLE [Квитанция] (
+
+	 [primaryKey] UNIQUEIDENTIFIER  NOT NULL,
+
+	 [Дата] date  NULL,
+
+	 [idTelePlus] INT  NULL,
+
+	 [Итого] VARCHAR(255)  NULL,
+
+	 [Помещение_m0] UNIQUEIDENTIFIER  NOT NULL,
+
+	 [УправляющаяКомпания_m0] UNIQUEIDENTIFIER  NOT NULL,
+
+	 PRIMARY KEY ([primaryKey]))
+
+
+CREATE TABLE [СтрокаКвитанции] (
+
+	 [primaryKey] UNIQUEIDENTIFIER  NOT NULL,
+
+	 [Расход] INT  NULL,
+
+	 [Единица] VARCHAR(255)  NULL,
+
+	 [Сумма] INT  NULL,
+
+	 [Расшифровка] VARCHAR(255)  NULL,
+
+	 [Комментарий] VARCHAR(255)  NULL,
+
+	 [ПоказаниеПУ_m0] UNIQUEIDENTIFIER  NOT NULL,
+
+	 [Тариф_m0] UNIQUEIDENTIFIER  NOT NULL,
+
+	 [Квитанция_m0] UNIQUEIDENTIFIER  NOT NULL,
+
+	 PRIMARY KEY ([primaryKey]))
+
+
+CREATE TABLE [STORMNETLOCKDATA] (
+
+	 [LockKey] VARCHAR(300)  NOT NULL,
+
+	 [UserName] VARCHAR(300)  NOT NULL,
+
+	 [LockDate] DATETIME  NULL,
+
+	 PRIMARY KEY ([LockKey]))
+
+
+CREATE TABLE [STORMSETTINGS] (
+
+	 [primaryKey] uniqueidentifier  NOT NULL,
+
+	 [Module] varchar(1000)  NULL,
+
+	 [Name] varchar(255)  NULL,
+
+	 [Value] text  NULL,
+
+	 [User] varchar(255)  NULL,
+
+	 PRIMARY KEY ([primaryKey]))
+
+
+CREATE TABLE [STORMAdvLimit] (
+
+	 [primaryKey] uniqueidentifier  NOT NULL,
+
+	 [User] varchar(255)  NULL,
+
+	 [Published] bit  NULL,
+
+	 [Module] varchar(255)  NULL,
+
+	 [Name] varchar(255)  NULL,
+
+	 [Value] text  NULL,
+
+	 [HotKeyData] int  NULL,
+
+	 PRIMARY KEY ([primaryKey]))
+
+
+CREATE TABLE [STORMFILTERSETTING] (
+
+	 [primaryKey] uniqueidentifier  NOT NULL,
+
+	 [Name] varchar(255)  NOT NULL,
+
+	 [DataObjectView] varchar(255)  NOT NULL,
+
+	 PRIMARY KEY ([primaryKey]))
+
+
+CREATE TABLE [STORMWEBSEARCH] (
+
+	 [primaryKey] uniqueidentifier  NOT NULL,
+
+	 [Name] varchar(255)  NOT NULL,
+
+	 [Order] INT  NOT NULL,
+
+	 [PresentView] varchar(255)  NOT NULL,
+
+	 [DetailedView] varchar(255)  NOT NULL,
+
+	 [FilterSetting_m0] uniqueidentifier  NOT NULL,
+
+	 PRIMARY KEY ([primaryKey]))
+
+
+CREATE TABLE [STORMFILTERDETAIL] (
+
+	 [primaryKey] uniqueidentifier  NOT NULL,
+
+	 [Caption] varchar(255)  NOT NULL,
+
+	 [DataObjectView] varchar(255)  NOT NULL,
+
+	 [ConnectMasterProp] varchar(255)  NOT NULL,
+
+	 [OwnerConnectProp] varchar(255)  NULL,
+
+	 [FilterSetting_m0] uniqueidentifier  NOT NULL,
+
+	 PRIMARY KEY ([primaryKey]))
+
+
+CREATE TABLE [STORMFILTERLOOKUP] (
+
+	 [primaryKey] uniqueidentifier  NOT NULL,
+
+	 [DataObjectType] varchar(255)  NOT NULL,
+
+	 [Container] varchar(255)  NULL,
+
+	 [ContainerTag] varchar(255)  NULL,
+
+	 [FieldsToView] varchar(255)  NULL,
+
+	 [FilterSetting_m0] uniqueidentifier  NOT NULL,
+
+	 PRIMARY KEY ([primaryKey]))
+
+
+CREATE TABLE [UserSetting] (
+
+	 [primaryKey] uniqueidentifier  NOT NULL,
+
+	 [AppName] varchar(256)  NULL,
+
+	 [UserName] varchar(512)  NULL,
+
+	 [UserGuid] uniqueidentifier  NULL,
+
+	 [ModuleName] varchar(1024)  NULL,
+
+	 [ModuleGuid] uniqueidentifier  NULL,
+
+	 [SettName] varchar(256)  NULL,
+
+	 [SettGuid] uniqueidentifier  NULL,
+
+	 [SettLastAccessTime] DATETIME  NULL,
+
+	 [StrVal] varchar(256)  NULL,
+
+	 [TxtVal] varchar(max)  NULL,
+
+	 [IntVal] int  NULL,
+
+	 [BoolVal] bit  NULL,
+
+	 [GuidVal] uniqueidentifier  NULL,
+
+	 [DecimalVal] decimal(20,10)  NULL,
+
+	 [DateTimeVal] DATETIME  NULL,
+
+	 PRIMARY KEY ([primaryKey]))
+
+
+CREATE TABLE [ApplicationLog] (
+
+	 [primaryKey] uniqueidentifier  NOT NULL,
+
+	 [Category] varchar(64)  NULL,
+
+	 [EventId] INT  NULL,
+
+	 [Priority] INT  NULL,
+
+	 [Severity] varchar(32)  NULL,
+
+	 [Title] varchar(256)  NULL,
+
+	 [Timestamp] DATETIME  NULL,
+
+	 [MachineName] varchar(32)  NULL,
+
+	 [AppDomainName] varchar(512)  NULL,
+
+	 [ProcessId] varchar(256)  NULL,
+
+	 [ProcessName] varchar(512)  NULL,
+
+	 [ThreadName] varchar(512)  NULL,
+
+	 [Win32ThreadId] varchar(128)  NULL,
+
+	 [Message] varchar(2500)  NULL,
+
+	 [FormattedMessage] varchar(max)  NULL,
+
+	 PRIMARY KEY ([primaryKey]))
+
+
+
+
+ ALTER TABLE [Тариф] ADD CONSTRAINT [Тариф_FВидУслуги_0] FOREIGN KEY ([ВидУслуги_m0]) REFERENCES [ВидУслуги]
+CREATE INDEX Тариф_IВидУслуги_m0 on [Тариф] ([ВидУслуги_m0])
+
+ ALTER TABLE [Тариф] ADD CONSTRAINT [Тариф_FПоставщик_0] FOREIGN KEY ([Поставщик_m0]) REFERENCES [Поставщик]
+CREATE INDEX Тариф_IПоставщик_m0 on [Тариф] ([Поставщик_m0])
+
+ ALTER TABLE [ПоказаниеПУ] ADD CONSTRAINT [ПоказаниеПУ_FПриборУчета_0] FOREIGN KEY ([ПриборУчета_m0]) REFERENCES [ПриборУчета]
+CREATE INDEX ПоказаниеПУ_IПриборУчета_m0 on [ПоказаниеПУ] ([ПриборУчета_m0])
+
+ ALTER TABLE [ПомещениеСобственника] ADD CONSTRAINT [ПомещениеСобственника_FПомещение_0] FOREIGN KEY ([Помещение_m0]) REFERENCES [Помещение]
+CREATE INDEX ПомещениеСобственника_IПомещение_m0 on [ПомещениеСобственника] ([Помещение_m0])
+
+ ALTER TABLE [ПомещениеСобственника] ADD CONSTRAINT [ПомещениеСобственника_FСобственник_0] FOREIGN KEY ([Собственник_m0]) REFERENCES [Собственник]
+CREATE INDEX ПомещениеСобственника_IСобственник_m0 on [ПомещениеСобственника] ([Собственник_m0])
+
+ ALTER TABLE [Помещение] ADD CONSTRAINT [Помещение_FДом_0] FOREIGN KEY ([Дом_m0]) REFERENCES [Дом]
+CREATE INDEX Помещение_IДом_m0 on [Помещение] ([Дом_m0])
+
+ ALTER TABLE [Помещение] ADD CONSTRAINT [Помещение_FАдресныйРеестр_0] FOREIGN KEY ([АдресныйРеестр_m0]) REFERENCES [АдресныйРеестр]
+CREATE INDEX Помещение_IАдресныйРеестр_m0 on [Помещение] ([АдресныйРеестр_m0])
+
+ ALTER TABLE [Помещение] ADD CONSTRAINT [Помещение_FСобственник_0] FOREIGN KEY ([Собственник_m0]) REFERENCES [Собственник]
+CREATE INDEX Помещение_IСобственник_m0 on [Помещение] ([Собственник_m0])
+
+ ALTER TABLE [Дом] ADD CONSTRAINT [Дом_FУправляющаяКомпания_0] FOREIGN KEY ([УправляющаяКомпания_m0]) REFERENCES [УправляющаяКомпания]
+CREATE INDEX Дом_IУправляющаяКомпания_m0 on [Дом] ([УправляющаяКомпания_m0])
+
+ ALTER TABLE [Дом] ADD CONSTRAINT [Дом_FАдресныйРеестр_0] FOREIGN KEY ([АдресныйРеестр_m0]) REFERENCES [АдресныйРеестр]
+CREATE INDEX Дом_IАдресныйРеестр_m0 on [Дом] ([АдресныйРеестр_m0])
+
+ ALTER TABLE [Сотрудник] ADD CONSTRAINT [Сотрудник_FУправляющаяКомпания_0] FOREIGN KEY ([УправляющаяКомпания_m0]) REFERENCES [УправляющаяКомпания]
+CREATE INDEX Сотрудник_IУправляющаяКомпания_m0 on [Сотрудник] ([УправляющаяКомпания_m0])
+
+ ALTER TABLE [ДомПоставщик] ADD CONSTRAINT [ДомПоставщик_FДом_0] FOREIGN KEY ([Дом_m0]) REFERENCES [Дом]
+CREATE INDEX ДомПоставщик_IДом_m0 on [ДомПоставщик] ([Дом_m0])
+
+ ALTER TABLE [ДомПоставщик] ADD CONSTRAINT [ДомПоставщик_FПоставщик_0] FOREIGN KEY ([Поставщик_m0]) REFERENCES [Поставщик]
+CREATE INDEX ДомПоставщик_IПоставщик_m0 on [ДомПоставщик] ([Поставщик_m0])
+
+ ALTER TABLE [ПриборУчета] ADD CONSTRAINT [ПриборУчета_FМодельПУ_0] FOREIGN KEY ([МодельПУ_m0]) REFERENCES [МодельПУ]
+CREATE INDEX ПриборУчета_IМодельПУ_m0 on [ПриборУчета] ([МодельПУ_m0])
+
+ ALTER TABLE [ПриборУчета] ADD CONSTRAINT [ПриборУчета_FПомещение_0] FOREIGN KEY ([Помещение_m0]) REFERENCES [Помещение]
+CREATE INDEX ПриборУчета_IПомещение_m0 on [ПриборУчета] ([Помещение_m0])
+
+ ALTER TABLE [Квитанция] ADD CONSTRAINT [Квитанция_FПомещение_0] FOREIGN KEY ([Помещение_m0]) REFERENCES [Помещение]
+CREATE INDEX Квитанция_IПомещение_m0 on [Квитанция] ([Помещение_m0])
+
+ ALTER TABLE [Квитанция] ADD CONSTRAINT [Квитанция_FУправляющаяКомпания_0] FOREIGN KEY ([УправляющаяКомпания_m0]) REFERENCES [УправляющаяКомпания]
+CREATE INDEX Квитанция_IУправляющаяКомпания_m0 on [Квитанция] ([УправляющаяКомпания_m0])
+
+ ALTER TABLE [СтрокаКвитанции] ADD CONSTRAINT [СтрокаКвитанции_FПоказаниеПУ_0] FOREIGN KEY ([ПоказаниеПУ_m0]) REFERENCES [ПоказаниеПУ]
+CREATE INDEX СтрокаКвитанции_IПоказаниеПУ_m0 on [СтрокаКвитанции] ([ПоказаниеПУ_m0])
+
+ ALTER TABLE [СтрокаКвитанции] ADD CONSTRAINT [СтрокаКвитанции_FТариф_0] FOREIGN KEY ([Тариф_m0]) REFERENCES [Тариф]
+CREATE INDEX СтрокаКвитанции_IТариф_m0 on [СтрокаКвитанции] ([Тариф_m0])
+
+ ALTER TABLE [СтрокаКвитанции] ADD CONSTRAINT [СтрокаКвитанции_FКвитанция_0] FOREIGN KEY ([Квитанция_m0]) REFERENCES [Квитанция]
+CREATE INDEX СтрокаКвитанции_IКвитанция_m0 on [СтрокаКвитанции] ([Квитанция_m0])
+
+ ALTER TABLE [STORMWEBSEARCH] ADD CONSTRAINT [STORMWEBSEARCH_FSTORMFILTERSETTING_0] FOREIGN KEY ([FilterSetting_m0]) REFERENCES [STORMFILTERSETTING]
+
+ ALTER TABLE [STORMFILTERDETAIL] ADD CONSTRAINT [STORMFILTERDETAIL_FSTORMFILTERSETTING_0] FOREIGN KEY ([FilterSetting_m0]) REFERENCES [STORMFILTERSETTING]
+
+ ALTER TABLE [STORMFILTERLOOKUP] ADD CONSTRAINT [STORMFILTERLOOKUP_FSTORMFILTERSETTING_0] FOREIGN KEY ([FilterSetting_m0]) REFERENCES [STORMFILTERSETTING]
+
---
+++ SQL/PostgreSql.create.sql
@@ -0,0 +1,518 @@
+﻿
+
+
+
+
+CREATE TABLE Поставщик (
+
+ primaryKey UUID NOT NULL,
+
+ ИНН VARCHAR(255) NULL,
+
+ Наименование VARCHAR(255) NULL,
+
+ РасчетныйСчет INT NULL,
+
+ Описание VARCHAR(255) NULL,
+
+ PRIMARY KEY (primaryKey));
+
+
+
+CREATE TABLE ВидУслуги (
+
+ primaryKey UUID NOT NULL,
+
+ Наименование VARCHAR(255) NULL,
+
+ Ресурс VARCHAR(255) NULL,
+
+ Актуальность BOOLEAN NULL,
+
+ PRIMARY KEY (primaryKey));
+
+
+
+CREATE TABLE Собственник (
+
+ primaryKey UUID NOT NULL,
+
+ ФИО VARCHAR(255) NULL,
+
+ идТелеПлюс INT NULL,
+
+ PRIMARY KEY (primaryKey));
+
+
+
+CREATE TABLE Тариф (
+
+ primaryKey UUID NOT NULL,
+
+ Наименование VARCHAR(255) NULL,
+
+ Описание VARCHAR(255) NULL,
+
+ ДатаНачала DATE NULL,
+
+ ДатаОкончания DATE NULL,
+
+ Единица VARCHAR(255) NULL,
+
+ Формула VARCHAR(255) NULL,
+
+ Стоимость VARCHAR(255) NULL,
+
+ ВидУслуги_m0 UUID NOT NULL,
+
+ Поставщик_m0 UUID NOT NULL,
+
+ PRIMARY KEY (primaryKey));
+
+
+
+CREATE TABLE ПоказаниеПУ (
+
+ primaryKey UUID NOT NULL,
+
+ Значение INT NULL,
+
+ Дата DATE NULL,
+
+ ПолученоАвтоматически BOOLEAN NULL,
+
+ ПриборУчета_m0 UUID NOT NULL,
+
+ PRIMARY KEY (primaryKey));
+
+
+
+CREATE TABLE ПомещениеСобственника (
+
+ primaryKey UUID NOT NULL,
+
+ ОсновнойСобственник VARCHAR(255) NULL,
+
+ ДоляСобственности VARCHAR(255) NULL,
+
+ Помещение_m0 UUID NOT NULL,
+
+ Собственник_m0 UUID NOT NULL,
+
+ PRIMARY KEY (primaryKey));
+
+
+
+CREATE TABLE Помещение (
+
+ primaryKey UUID NOT NULL,
+
+ Площадь INT NULL,
+
+ ТипПомещения VARCHAR(255) NULL,
+
+ КолвоПроживающих VARCHAR(255) NULL,
+
+ Дом_m0 UUID NOT NULL,
+
+ АдресныйРеестр_m0 UUID NOT NULL,
+
+ Собственник_m0 UUID NOT NULL,
+
+ PRIMARY KEY (primaryKey));
+
+
+
+CREATE TABLE Дом (
+
+ primaryKey UUID NOT NULL,
+
+ Address VARCHAR(255) NULL,
+
+ summS INT NULL,
+
+ УправляющаяКомпания_m0 UUID NOT NULL,
+
+ АдресныйРеестр_m0 UUID NOT NULL,
+
+ PRIMARY KEY (primaryKey));
+
+
+
+CREATE TABLE МодельПУ (
+
+ primaryKey UUID NOT NULL,
+
+ Наименование VARCHAR(255) NULL,
+
+ Производитель VARCHAR(255) NULL,
+
+ Актуальность BOOLEAN NULL,
+
+ PRIMARY KEY (primaryKey));
+
+
+
+CREATE TABLE Сотрудник (
+
+ primaryKey UUID NOT NULL,
+
+ ФИО VARCHAR(255) NULL,
+
+ УправляющаяКомпания_m0 UUID NOT NULL,
+
+ PRIMARY KEY (primaryKey));
+
+
+
+CREATE TABLE УправляющаяКомпания (
+
+ primaryKey UUID NOT NULL,
+
+ Наименование VARCHAR(255) NULL,
+
+ ИНН VARCHAR(255) NULL,
+
+ PRIMARY KEY (primaryKey));
+
+
+
+CREATE TABLE ДомПоставщик (
+
+ primaryKey UUID NOT NULL,
+
+ Дом_m0 UUID NOT NULL,
+
+ Поставщик_m0 UUID NOT NULL,
+
+ PRIMARY KEY (primaryKey));
+
+
+
+CREATE TABLE ПриборУчета (
+
+ primaryKey UUID NOT NULL,
+
+ Единица VARCHAR(255) NULL,
+
+ ДатаУстановки DATE NULL,
+
+ ДатаСнятия DATE NULL,
+
+ Актуальность BOOLEAN NULL,
+
+ АвтоматическаяПередача BOOLEAN NULL,
+
+ МодельПУ_m0 UUID NOT NULL,
+
+ Помещение_m0 UUID NOT NULL,
+
+ PRIMARY KEY (primaryKey));
+
+
+
+CREATE TABLE АдресныйРеестр (
+
+ primaryKey UUID NOT NULL,
+
+ PRIMARY KEY (primaryKey));
+
+
+
+CREATE TABLE Квитанция (
+
+ primaryKey UUID NOT NULL,
+
+ Дата DATE NULL,
+
+ idTelePlus INT NULL,
+
+ Итого VARCHAR(255) NULL,
+
+ Помещение_m0 UUID NOT NULL,
+
+ УправляющаяКомпания_m0 UUID NOT NULL,
+
+ PRIMARY KEY (primaryKey));
+
+
+
+CREATE TABLE СтрокаКвитанции (
+
+ primaryKey UUID NOT NULL,
+
+ Расход INT NULL,
+
+ Единица VARCHAR(255) NULL,
+
+ Сумма INT NULL,
+
+ Расшифровка VARCHAR(255) NULL,
+
+ Комментарий VARCHAR(255) NULL,
+
+ ПоказаниеПУ_m0 UUID NOT NULL,
+
+ Тариф_m0 UUID NOT NULL,
+
+ Квитанция_m0 UUID NOT NULL,
+
+ PRIMARY KEY (primaryKey));
+
+
+
+CREATE TABLE STORMNETLOCKDATA (
+
+ LockKey VARCHAR(300) NOT NULL,
+
+ UserName VARCHAR(300) NOT NULL,
+
+ LockDate TIMESTAMP(3) NULL,
+
+ PRIMARY KEY (LockKey));
+
+
+
+CREATE TABLE STORMSETTINGS (
+
+ primaryKey UUID NOT NULL,
+
+ Module VARCHAR(1000) NULL,
+
+ Name VARCHAR(255) NULL,
+
+ Value TEXT NULL,
+
+ "User" VARCHAR(255) NULL,
+
+ PRIMARY KEY (primaryKey));
+
+
+
+CREATE TABLE STORMAdvLimit (
+
+ primaryKey UUID NOT NULL,
+
+ "User" VARCHAR(255) NULL,
+
+ Published BOOLEAN NULL,
+
+ Module VARCHAR(255) NULL,
+
+ Name VARCHAR(255) NULL,
+
+ Value TEXT NULL,
+
+ HotKeyData INT NULL,
+
+ PRIMARY KEY (primaryKey));
+
+
+
+CREATE TABLE STORMFILTERSETTING (
+
+ primaryKey UUID NOT NULL,
+
+ Name VARCHAR(255) NOT NULL,
+
+ DataObjectView VARCHAR(255) NOT NULL,
+
+ PRIMARY KEY (primaryKey));
+
+
+
+CREATE TABLE STORMWEBSEARCH (
+
+ primaryKey UUID NOT NULL,
+
+ Name VARCHAR(255) NOT NULL,
+
+ "Order" INT NOT NULL,
+
+ PresentView VARCHAR(255) NOT NULL,
+
+ DetailedView VARCHAR(255) NOT NULL,
+
+ FilterSetting_m0 UUID NOT NULL,
+
+ PRIMARY KEY (primaryKey));
+
+
+
+CREATE TABLE STORMFILTERDETAIL (
+
+ primaryKey UUID NOT NULL,
+
+ Caption VARCHAR(255) NOT NULL,
+
+ DataObjectView VARCHAR(255) NOT NULL,
+
+ ConnectMasterProp VARCHAR(255) NOT NULL,
+
+ OwnerConnectProp VARCHAR(255) NULL,
+
+ FilterSetting_m0 UUID NOT NULL,
+
+ PRIMARY KEY (primaryKey));
+
+
+
+CREATE TABLE STORMFILTERLOOKUP (
+
+ primaryKey UUID NOT NULL,
+
+ DataObjectType VARCHAR(255) NOT NULL,
+
+ Container VARCHAR(255) NULL,
+
+ ContainerTag VARCHAR(255) NULL,
+
+ FieldsToView VARCHAR(255) NULL,
+
+ FilterSetting_m0 UUID NOT NULL,
+
+ PRIMARY KEY (primaryKey));
+
+
+
+CREATE TABLE UserSetting (
+
+ primaryKey UUID NOT NULL,
+
+ AppName VARCHAR(256) NULL,
+
+ UserName VARCHAR(512) NULL,
+
+ UserGuid UUID NULL,
+
+ ModuleName VARCHAR(1024) NULL,
+
+ ModuleGuid UUID NULL,
+
+ SettName VARCHAR(256) NULL,
+
+ SettGuid UUID NULL,
+
+ SettLastAccessTime TIMESTAMP(3) NULL,
+
+ StrVal VARCHAR(256) NULL,
+
+ TxtVal TEXT NULL,
+
+ IntVal INT NULL,
+
+ BoolVal BOOLEAN NULL,
+
+ GuidVal UUID NULL,
+
+ DecimalVal DECIMAL(20,10) NULL,
+
+ DateTimeVal TIMESTAMP(3) NULL,
+
+ PRIMARY KEY (primaryKey));
+
+
+
+CREATE TABLE ApplicationLog (
+
+ primaryKey UUID NOT NULL,
+
+ Category VARCHAR(64) NULL,
+
+ EventId INT NULL,
+
+ Priority INT NULL,
+
+ Severity VARCHAR(32) NULL,
+
+ Title VARCHAR(256) NULL,
+
+ Timestamp TIMESTAMP(3) NULL,
+
+ MachineName VARCHAR(32) NULL,
+
+ AppDomainName VARCHAR(512) NULL,
+
+ ProcessId VARCHAR(256) NULL,
+
+ ProcessName VARCHAR(512) NULL,
+
+ ThreadName VARCHAR(512) NULL,
+
+ Win32ThreadId VARCHAR(128) NULL,
+
+ Message VARCHAR(2500) NULL,
+
+ FormattedMessage TEXT NULL,
+
+ PRIMARY KEY (primaryKey));
+
+
+
+
+ ALTER TABLE Тариф ADD CONSTRAINT FK942d315532514221a9cb53532c73ef7e FOREIGN KEY (ВидУслуги_m0) REFERENCES ВидУслуги; 
+CREATE INDEX Index4efe9f21f1565b469b033b23b22cc953d6c41527 on Тариф (ВидУслуги_m0); 
+
+ ALTER TABLE Тариф ADD CONSTRAINT FK87ee952142c4427487fe43d7f3bb66dd FOREIGN KEY (Поставщик_m0) REFERENCES Поставщик; 
+CREATE INDEX Index33c10eeb7288c970ebdf38051c0eb48f55cb7bff on Тариф (Поставщик_m0); 
+
+ ALTER TABLE ПоказаниеПУ ADD CONSTRAINT FK35049e202fbb4a309e0621cf13d55ecf FOREIGN KEY (ПриборУчета_m0) REFERENCES ПриборУчета; 
+CREATE INDEX Indexd6cce9d38823a1c432711a50b19373503b5d68d2 on ПоказаниеПУ (ПриборУчета_m0); 
+
+ ALTER TABLE ПомещениеСобственника ADD CONSTRAINT FKb4a37014db1b46478c949007057eaa60 FOREIGN KEY (Помещение_m0) REFERENCES Помещение; 
+CREATE INDEX Index61299f76ff53ff3270ca002f2a3d3427da2213a8 on ПомещениеСобственника (Помещение_m0); 
+
+ ALTER TABLE ПомещениеСобственника ADD CONSTRAINT FK2f0d13611ba948a28f3821d28adce791 FOREIGN KEY (Собственник_m0) REFERENCES Собственник; 
+CREATE INDEX Index0fe3d46f06fe93ab18c33cae122a904d334f71c3 on ПомещениеСобственника (Собственник_m0); 
+
+ ALTER TABLE Помещение ADD CONSTRAINT FK9052dd001c60449cb8958e0a615e58b9 FOREIGN KEY (Дом_m0) REFERENCES Дом; 
+CREATE INDEX Indexf97e664ca029e850560b5496b12896dde2455ab2 on Помещение (Дом_m0); 
+
+ ALTER TABLE Помещение ADD CONSTRAINT FK0c27aedb71d84af0819e7098dc085e87 FOREIGN KEY (АдресныйРеестр_m0) REFERENCES АдресныйРеестр; 
+CREATE INDEX Indexfe72dfb9a9ea657d02bd301b6c7deb9364f43144 on Помещение (АдресныйРеестр_m0); 
+
+ ALTER TABLE Помещение ADD CONSTRAINT FK614b69d62351430c97fdde5ab808cacb FOREIGN KEY (Собственник_m0) REFERENCES Собственник; 
+CREATE INDEX Index88ae7359508e1b0f29ac3d6391b1ada245fdbe20 on Помещение (Собственник_m0); 
+
+ ALTER TABLE Дом ADD CONSTRAINT FK80ee70aeab374bcfa5a43403d1869692 FOREIGN KEY (УправляющаяКомпания_m0) REFERENCES УправляющаяКомпания; 
+CREATE INDEX Index8cc613dc87087d9dacaeec621207a2e90de170f1 on Дом (УправляющаяКомпания_m0); 
+
+ ALTER TABLE Дом ADD CONSTRAINT FK7847d7e8fe0f4379ba4b0d6759b2d99d FOREIGN KEY (АдресныйРеестр_m0) REFERENCES АдресныйРеестр; 
+CREATE INDEX Index849aee09df438a952692e001c656b0d98898d9d4 on Дом (АдресныйРеестр_m0); 
+
+ ALTER TABLE Сотрудник ADD CONSTRAINT FK5c79d3f5f3af4c77ad97487e162de281 FOREIGN KEY (УправляющаяКомпания_m0) REFERENCES УправляющаяКомпания; 
+CREATE INDEX Indexf69f79f9f32939c432502ebf4f1d9ff00132ef66 on Сотрудник (УправляющаяКомпания_m0); 
+
+ ALTER TABLE ДомПоставщик ADD CONSTRAINT FK542838f23c2f4df29dc5493ea5d6b183 FOREIGN KEY (Дом_m0) REFERENCES Дом; 
+CREATE INDEX Index395527b359b8a755046b8085885e36bc7ea4772a on ДомПоставщик (Дом_m0); 
+
+ ALTER TABLE ДомПоставщик ADD CONSTRAINT FK6536fda84f3e4b56b695ed673a764314 FOREIGN KEY (Поставщик_m0) REFERENCES Поставщик; 
+CREATE INDEX Index474328d8c86a68c4ecc2cdcbcb507d7272335bb7 on ДомПоставщик (Поставщик_m0); 
+
+ ALTER TABLE ПриборУчета ADD CONSTRAINT FKe1b82af5c10946acad08abba166bdb8f FOREIGN KEY (МодельПУ_m0) REFERENCES МодельПУ; 
+CREATE INDEX Index0b5e9eb2d31d78044e491bbfb1691aef8746951a on ПриборУчета (МодельПУ_m0); 
+
+ ALTER TABLE ПриборУчета ADD CONSTRAINT FK38ba02742fc548dbb84ae0046f93c5ea FOREIGN KEY (Помещение_m0) REFERENCES Помещение; 
+CREATE INDEX Index03e59a2c17a211fa9af9bb9fd79463203fcd2558 on ПриборУчета (Помещение_m0); 
+
+ ALTER TABLE Квитанция ADD CONSTRAINT FKd256ea4e7d8549ad8e5d14950492b2cc FOREIGN KEY (Помещение_m0) REFERENCES Помещение; 
+CREATE INDEX Index3b2475a1dea2a575569bb051ca87c1ad7c239647 on Квитанция (Помещение_m0); 
+
+ ALTER TABLE Квитанция ADD CONSTRAINT FK519e0cdfe1574d079e2025b997c266a8 FOREIGN KEY (УправляющаяКомпания_m0) REFERENCES УправляющаяКомпания; 
+CREATE INDEX Index8a726da6bb6ef494f0c35f58a7ff42b4efbb9625 on Квитанция (УправляющаяКомпания_m0); 
+
+ ALTER TABLE СтрокаКвитанции ADD CONSTRAINT FK6bffdcf2cae34328935e03718e736662 FOREIGN KEY (ПоказаниеПУ_m0) REFERENCES ПоказаниеПУ; 
+CREATE INDEX Index3b33ecdbea4a900de7aae2210c993278d855a0d8 on СтрокаКвитанции (ПоказаниеПУ_m0); 
+
+ ALTER TABLE СтрокаКвитанции ADD CONSTRAINT FK3022e73c2c484d0aada8e8154b166eff FOREIGN KEY (Тариф_m0) REFERENCES Тариф; 
+CREATE INDEX Index703ba542e585241f1ff80d29061fe379b2308d1f on СтрокаКвитанции (Тариф_m0); 
+
+ ALTER TABLE СтрокаКвитанции ADD CONSTRAINT FK46d7ab3f6e474a1e90948ed551474c7b FOREIGN KEY (Квитанция_m0) REFERENCES Квитанция; 
+CREATE INDEX Indexce88417d4864090c9df2be074537be19db2fb5a7 on СтрокаКвитанции (Квитанция_m0); 
+
+ ALTER TABLE STORMWEBSEARCH ADD CONSTRAINT FK4b597584140b44499b5e1e00b261b61b FOREIGN KEY (FilterSetting_m0) REFERENCES STORMFILTERSETTING; 
+
+ ALTER TABLE STORMFILTERDETAIL ADD CONSTRAINT FK862dbf3124de45b6b1965179c2f41cd2 FOREIGN KEY (FilterSetting_m0) REFERENCES STORMFILTERSETTING; 
+
+ ALTER TABLE STORMFILTERLOOKUP ADD CONSTRAINT FK201ce5b8247547be8f61648242334e24 FOREIGN KEY (FilterSetting_m0) REFERENCES STORMFILTERSETTING; 
+
